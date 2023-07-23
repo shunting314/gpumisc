9e1cef8d7f81a15eaff223f03e40a1b8bd6cdf42
Shunting Zhang <shunting@fb.com>
2023-07-22 22:22:07 -0700

    more properties

--- prop.cu
+++ prop.cu
@@ -8,6 +8,17 @@ void showDeviceProperties(int devid) {
   printf("  #SM: %d\n", prop.multiProcessorCount);
   printf("  #global mem: %.3fGB (%.3fGiB)\n", (double) prop.totalGlobalMem / 1e9, (double) prop.totalGlobalMem / (1 << 30));
   printf("  #L2$: %.3fMB (%.3fMiB)\n", (double) prop.l2CacheSize / 1e6, (double) prop.l2CacheSize / (1 << 20));
+  printf("  maxThreadsPerBlock %d\n", prop.maxThreadsPerBlock);
+  printf("  clockRate %d\n", prop.clockRate); // core clock rate
+  for (int i = 0; i < 3; ++i) {
+    printf("  maxThreadsDim[%d] %d\n", i, prop.maxThreadsDim[i]);
+  }
+  for (int i = 0; i < 3; ++i) {
+    printf("  maxGridSize[%d] %d\n", i, prop.maxGridSize[i]);
+  }
+  printf("  regsPerBlock %d\n", prop.regsPerBlock);
+  printf("  regsPerMultiprocessor %d\n", prop.regsPerMultiprocessor);
+  printf("  warpSize %d\n", prop.warpSize);
 }
 
 int main(void) {
